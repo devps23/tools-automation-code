4a6ffa3b5af96fc996f7338827599f1753460a2f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-12-22 14:05:51 +0530

    1.create an instance 2.create a vpc 3.create two subnets for each zone 4.peer connection between two vpc ids

--- tools/data.tf
+++ tools/data.tf
@@ -3,3 +3,14 @@ data "aws_ami" "ami" {
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
 }
+# get existing assume role policy from service
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
