fb6e6904447df3eb1ff9e210cd99de7b9ae1c4a4
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-16 13:38:06 +0530

    get secrets from vault

---
+++ jenkins.sh
@@ -0,0 +1,4 @@
+#bin/bash
+ansible-playbook -i jenkins-internal.pdevops72.online:8080, tools.yml -e tool_name=jenkins -e ansible_username=ec2-user -e ansible_password=DevOps321 -e jenkins_setup=true -e jenkins_job=true
+
+# here jenkins_setup=true and jenkins_job=true means dynamically to pass bool value by default it is false
