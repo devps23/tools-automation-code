c2aa50b604aa1dd36f8abaaa14840b052633f4d1
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-10 14:30:09 +0530

    jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 #bin/bash
-ansible-playbook -i jenkins-internal.pdevops72.online:8080, tools.yml -e tool_name=jenkins -e ansible_username=ec2-user -e ansible_password=DevOps321 -e jenkins_setup=true -e jenkins_job=true -e vault_token="hvs.T8WnPWmYmGZkPYlGAHXVmebk"
+ansible-playbook -i jenkins-internal.pdevops72.online:8080, tools.yml -e tool_name=jenkins -e ansible_username=ec2-user -e ansible_password=DevOps321 -e jenkins_setup=true -e jenkins_job=false -e vault_token="hvs.T8WnPWmYmGZkPYlGAHXVmebk"
 
 # here jenkins_setup=true and jenkins_job=true means dynamically to pass bool value by default it is false
